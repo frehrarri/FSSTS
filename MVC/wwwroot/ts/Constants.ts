3e7d13912327383937a064edc5496fecf837c968
frehroshaka <mitchellfrehr@gmail.com>
2024-04-11 21:15:05 -0400

    added missing ts files and mostly finished the registration client validation. just need to find out why im unable to initialize arrays with ts for some reason. added Constants.ts to be used by all typescript files.

---
+++ MVC/wwwroot/ts/Constants.ts
@@ -0,0 +1,29 @@
+﻿    export class Constants {
+
+        //Configs
+        readonly SiteURL = "https://localhost:7241/";
+
+        //Login
+        readonly IncorrectPassword = "Incorrect Password";
+        readonly IncorrectUsername = "Incorrect Username";
+
+        //Register
+        readonly UsernameNotValid = "Invalid Username";
+        readonly UsernameExists = "{0} already exists. Please select another name";
+        readonly IncorrectUsernameLength = "Username must be between 4 and 20 characters";
+        readonly UsernameCharactersInvalid = "Username can not have special characters";
+        readonly PasswordNotValid = "Incorrect password";
+        readonly IncorrectPasswordLength = "Password must be between 8 and 20 characters";
+        readonly PasswordCharactersNotValid = "Password must contain letters, numbers, and special characters";
+        readonly RecoveryQuestionEmpty = "Please enter a question you will remember the answer to";
+        readonly RecoveryAnswerEmpty = "Please enter an answer for the recovery question";
+        readonly FirstNameEmpty = "Please enter your first name";
+        readonly LastNameEmpty = "Please enter your last name";
+        readonly AddressEmpty = "Please enter your address";
+        readonly EmailEmpty = "Please enter your email";
+        readonly InvalidEmailFormat = "Invalid email format";
+        readonly PhoneEmpty = "Please enter your phone number";
+        readonly InvalidPhoneFormat = "Phone numbers can only contain numbers";
+        readonly InvalidPhoneLength = "Phone numbers must contain 10 digits";
+    }
+
